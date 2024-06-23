b39b87445305987b470929c60d1838767a68eea3
Fantom52r <fantom52rs@gmail.com>
2024-06-23 06:14:31 +0300

    HW-4 -INPUT

--- src/components/registration/Registration.jsx
+++ src/components/registration/Registration.jsx
@@ -5,24 +5,24 @@ import * as S from '../../styles/common.styled'
 
 const Registration = () => {
   return (
-    <div className="container-signup">
-    <div className="modal">
-<div className="modal__block">
-  <div className="modal__ttl">
-    <h2>Регистрация</h2>
-  </div>
-  <S.moda>
-    <input className="modal__input first-name" type="text" name="first-name" id="first-name" placeholder="Имя"/>
-    <input className="modal__input login" type="text" name="login" id="loginReg" placeholder="Эл. почта"/>
-    <input className="modal__input password-first" type="password" name="password" id="passwordFirst" placeholder="Пароль"/>
-    <button className="modal__btn-signup-ent _hover01" id="SignUpEnter"><a href="../main.html">Зарегистрироваться</a> </button>
-    <div className="modal__form-group">
-      <p>Уже есть аккаунт?  <Link to = {paths.LOGIN}>Войдите здесь</Link></p>
-    </div>
-  </form>
-</div>
-</div>
-</div>
+    <S.ModalContainer>
+      <S.Modal>
+        <S.ModalBlock>
+          <S.ModalTitle>
+            <h2>Регистрация</h2>
+          </S.ModalTitle>
+          <S.ModalForm>
+            <input type="text" name="first-name" id="first-name" placeholder="Имя" />
+            <input className="modal__input login" type="text" name="login" id="loginReg" placeholder="Эл. почта" />
+            <input className="modal__input password-first" type="password" name="password" id="passwordFirst" placeholder="Пароль" />
+            <S.ModalEnterButton id="SignUpEnter"><a href="../main.html">Зарегистрироваться</a> </S.ModalEnterButton>
+            <S.ModalFormGroup>
+              <p>Уже есть аккаунт?  <Link to={paths.LOGIN}>Войдите здесь</Link></p>
+            </S.ModalFormGroup>
+          </S.ModalForm>
+        </S.ModalBlock>
+      </S.Modal>
+    </S.ModalContainer>
   )
 }
 
--- src/styles/common.styled.js
+++ src/styles/common.styled.js
@@ -39,14 +39,34 @@ export const Hover03 = css`
 `;
 
 export const Modal = styled.div`
-
+width: 100%;
+  height: 100%;
+  min-width: 320px;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `
 export const ModalBlock = styled.div`
-
+display: block;
+  margin: 0 auto;
+  background-color: #FFFFFF;
+  max-width: 368px;
+  width: 100%;
+  padding: 50px 60px;
+  border-radius: 10px;
+  border: 0.7px solid #D4DBE5;
+  box-shadow: 0px 4px 67px -12px rgba(0, 0, 0, 0.13);
 `
 
 export const ModalTitle = styled.div`
-
+text-align: center;
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 30px;
+  letter-spacing: -0.6px;
+  margin-bottom: 20px;
 `
 export const ModalForm = styled.form`
  width: 100%;
@@ -76,12 +96,37 @@ width: 100%;
 `
 
 export const ModalFormGroup = styled.div`
-
+ text-align: center;
+ & p {
+  color: rgba(148, 166, 190, 0.4);
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 150%;
+  letter-spacing: -0.14px;
+ }
 `
 export const ModalEnterButton = styded.button`
-
+  width: 100%;
+  height: 30px;
+  background-color: #565EEF;
+  border-radius: 4px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  border: none;
+  outline: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  line-height: 21px;
+  font-weight: 500;
+  letter-spacing: -0.14px;
+  color: #FFFFFF;
 `
 
 export const ModalContainer = styled.div`
-
+  display: block;
+  width: 100vw;
+  min-height: 100vh;
+  margin: 0 auto;
 `
